077091713ce1de311c895620ed6b597074fe2f31
Dani <24252795+SGD14@users.noreply.github.com>
2023-01-10 18:12:31 +0100

    Polish prototypes

--- src/components/channel-nav/IconButton.jsx
+++ src/components/channel-nav/IconButton.jsx
@@ -1,6 +1,6 @@
 const IconButton = ({ icon }) => {
   return (
-    <div className="flex justify-center items-center w-8 h-9 hover:bg-neutral-700 transition-all rounded-md">
+    <div className="flex justify-center items-center w-8 h-9 hover:bg-neutral-700 transition-all rounded-md fill-zinc-300 cursor-pointer">
       {icon}
     </div>
   );
