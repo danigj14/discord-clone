e29b1e509a2bcd5f3b43db3aa02e879f68a4c180
Dani <24252795+SGD14@users.noreply.github.com>
2023-01-10 22:22:26 +0100

    Improve chat messages spacing

--- src/components/chat/message/HeadMessage.jsx
+++ src/components/chat/message/HeadMessage.jsx
@@ -1,11 +1,11 @@
 export default function HeadMessage({ content }) {
   return (
-    <div className="px-4 py-1 hover:bg-zinc-800 flex items-center gap-2">
-      <img
+    <div className="px-4 py-1 hover:bg-zinc-800 flex items-center">
+      <div className="w-14"><img
         src="https://www.leawo.com/blog/wp-content/uploads/2009/12/avatar8.jpg"
         className="w-10 h-10 rounded-full"
         alt="User Avatar"
-      />
+      /></div>
       <div className="text-sm">
         <div className="flex items-center gap-2">
           <div className="font-bold">Username</div>
--- src/components/chat/message/NormalMessage.jsx
+++ src/components/chat/message/NormalMessage.jsx
@@ -1,7 +1,7 @@
 export default function NormalMessage({ content }) {
   return (
-    <div className="px-4 py-1 hover:bg-zinc-800 flex items-center gap-2 group">
-      <div className="w-10 text-[0.6rem] text-zinc-400 opacity-0 group-hover:opacity-100">
+    <div className="px-4 py-1 hover:bg-zinc-800 flex items-center group">
+      <div className="w-14 text-[0.7rem] text-zinc-400 opacity-0 group-hover:opacity-100">
         5:40 PM
       </div>
       <div className="text-sm">{content}</div>
