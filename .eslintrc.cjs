f4e8122f031665fe5c1da69bafc74e12ab0f085c
Dani <24252795+SGD14@users.noreply.github.com>
2023-01-10 13:17:52 +0100

    Format all with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb',
-    'airbnb/react',
-  ],
-  overrides: [
-  ],
+  extends: ["airbnb", "airbnb/react"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+  plugins: ["react"],
+  rules: {},
 };
