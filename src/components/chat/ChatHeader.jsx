2c8eb5f7bccb27b2cec380306dd4029a22b5bbc5
Dani <24252795+SGD14@users.noreply.github.com>
2023-01-11 14:21:47 +0100

    Add missing icons and chat header search field

--- src/components/chat/ChatHeader.jsx
+++ src/components/chat/ChatHeader.jsx
@@ -1,9 +1,14 @@
+import { faHashtag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ChatHeaderMenu from "./ChatHeaderMenu";
 
 export default function ChatHeader({ name }) {
   return (
     <div className="px-4 py-4 border-b-2 border-zinc-800 flex">
-      <div className="flex-grow">{name}</div>
+      <div className="flex-grow flex items-center gap-2 text-zinc-100 font-bold text-sm select-none">
+        <FontAwesomeIcon className="h-5 text-zinc-400" icon={faHashtag} />
+        {name}
+      </div>
       <ChatHeaderMenu />
     </div>
   );
--- src/components/chat/ChatInput.jsx
+++ src/components/chat/ChatInput.jsx
@@ -1,18 +1,31 @@
+import {
+  faCirclePlus,
+  faFaceSmile,
+  faFile,
+  faGift,
+  faImage,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const iconButtonsClassName =
+  "h-5 text-zinc-300 hover:text-zinc-100 cursor-pointer";
+
 export default function ChatInput() {
   return (
     <div className="m-4 pr-4 flex items-center bg-zinc-600 rounded-lg">
-      <div className="text-3xl font-bold px-4 py-2 text-zinc-400 hover:text-zinc-200">
-        +
-      </div>
+      <FontAwesomeIcon
+        className="h-5 font-bold px-4 py-3 text-zinc-400 hover:text-zinc-200 cursor-pointer"
+        icon={faCirclePlus}
+      />
       <input
         className="w-4 bg-zinc-600 flex-grow focus:outline-none"
         placeholder="Message #Channel 1"
       />
-      <div className="flex gap-3">
-        <div>Gift</div>
-        <div>Gif</div>
-        <div>File</div>
-        <div>Icon</div>
+      <div className="flex gap-4 items-center">
+        <FontAwesomeIcon className={iconButtonsClassName} icon={faGift} />
+        <FontAwesomeIcon className={iconButtonsClassName} icon={faImage} />
+        <FontAwesomeIcon className={iconButtonsClassName} icon={faFile} />
+        <FontAwesomeIcon className={iconButtonsClassName} icon={faFaceSmile} />
       </div>
     </div>
   );
