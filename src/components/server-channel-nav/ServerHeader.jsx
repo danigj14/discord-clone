d96ee05f273f94cfc2ec3fa400462e96f33cc92c
Dani <24252795+SGD14@users.noreply.github.com>
2023-01-13 21:39:07 +0100

    Adjust channel nav and chat header heights so they match together nicely

--- src/components/server-channel-nav/ServerHeader.jsx
+++ src/components/server-channel-nav/ServerHeader.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function ServerHeader({ name }) {
   return (
     <button
-      className="px-4 py-4 flex justify-between items-center border-b-2 text-zinc-100 border-zinc-900 hover:bg-zinc-700 transition-colors cursor-pointer"
+      className="px-4 py-4 h-14 flex justify-between items-center border-b-2 text-zinc-100 border-zinc-900 hover:bg-zinc-700 transition-colors cursor-pointer"
       type="button"
     >
       <span>{name}</span>
