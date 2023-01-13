4adf15d5b304289e500b65d25f4fdbab2cf1eeda
Dani <24252795+SGD14@users.noreply.github.com>
2023-01-13 21:40:06 +0100

    Improve chat header border opacity

--- src/components/chat/ChatHeader.jsx
+++ src/components/chat/ChatHeader.jsx
@@ -4,7 +4,7 @@ import ChatHeaderMenu from "./ChatHeaderMenu";
 
 export default function ChatHeader({ name }) {
   return (
-    <div className="px-4 py-4 border-b-2 border-zinc-800 flex h-14">
+    <div className="px-4 py-4 border-b-2 border-zinc-800 border-opacity-50 flex h-14">
       <div className="flex-grow flex items-center gap-2 text-zinc-100 font-bold text-sm select-none">
         <FontAwesomeIcon className="h-5 text-zinc-400" icon={faHashtag} />
         {name}
