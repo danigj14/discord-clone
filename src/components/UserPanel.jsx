5067f73dc1eedacde6cc8bbd32e1cad5d613e8a8
Dani <24252795+SGD14@users.noreply.github.com>
2023-01-13 21:35:49 +0100

    Improve UserPanel background color

--- src/components/UserPanel.jsx
+++ src/components/UserPanel.jsx
@@ -7,7 +7,7 @@ import IconButton from "./IconButton";
 
 export default function UserPanel() {
   return (
-    <div className="flex gap-1 p-2 bg-neutral-800 items-center">
+    <div className="flex gap-1 p-2 bg-zinc-900 bg-opacity-50 items-center">
       <div className="flex-grow flex text-xs p-1 gap-2 items-center hover:bg-neutral-700 rounded-md transition-all cursor-pointer">
         <img
           src="https://www.leawo.com/blog/wp-content/uploads/2009/12/avatar8.jpg"
