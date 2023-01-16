916077f972b43e81f5c3c0aef9468e68e7714232
Dani <24252795+SGD14@users.noreply.github.com>
2023-01-16 23:40:52 +0100

    Remove JSON Server

--- src/components/UserPanel.test.jsx
+++ src/components/UserPanel.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import UserPanel from "../src/components/UserPanel";
-import UserContext from "../src/contexts/UserContext";
+import UserPanel from "./UserPanel";
+import UserContext from "../contexts/UserContext";
 
 const mockUser = {
   id: 1,
