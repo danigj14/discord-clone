0d95ee15731e34cb327550785c144d2fa97b05a9
Dani <24252795+SGD14@users.noreply.github.com>
2023-01-20 11:55:42 +0100

    Refactor chat components and create ServerChannelChat test

--- src/components/chat/ChatHeader.jsx
+++ src/components/chat/ChatHeader.jsx
@@ -1,6 +1,8 @@
-import { faHashtag } from "@fortawesome/free-solid-svg-icons";
+import { faBellSlash, faCircleQuestion, faComments, faHashtag, faInbox, faMagnifyingGlass, faThumbTack, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ChatHeaderMenu from "./ChatHeaderMenu";
+
+const iconButtonsClassName =
+  "h-5 text-zinc-300 hover:text-zinc-100 cursor-pointer";
 
 export default function ChatHeader({ name }) {
   return (
@@ -9,7 +11,27 @@ export default function ChatHeader({ name }) {
         <FontAwesomeIcon className="h-5 text-zinc-400" icon={faHashtag} />
         {name}
       </div>
-      <ChatHeaderMenu />
+      <div className="flex gap-4 items-center">
+        <FontAwesomeIcon className={iconButtonsClassName} icon={faComments} />
+        <FontAwesomeIcon className={iconButtonsClassName} icon={faBellSlash} />
+        <FontAwesomeIcon className={iconButtonsClassName} icon={faThumbTack} />
+        <FontAwesomeIcon className={iconButtonsClassName} icon={faUserGroup} />
+        <div className="bg-zinc-800 px-2 text-xs rounded-md flex items-center cursor-text group">
+          <input
+            placeholder="Search"
+            className="bg-transparent w-28 py-1 flex-grow outline-none focus:w-52 transition-all"
+          />
+          <FontAwesomeIcon
+            className="h-3 px-1 text-zinc-400"
+            icon={faMagnifyingGlass}
+          />
+        </div>
+        <FontAwesomeIcon className={iconButtonsClassName} icon={faInbox} />
+        <FontAwesomeIcon
+          className={iconButtonsClassName}
+          icon={faCircleQuestion}
+        />
+      </div>
     </div>
   );
 }
--- src/components/chat/ChatInput.jsx
+++ src/components/chat/ChatInput.jsx
@@ -6,21 +6,35 @@ import {
   faImage,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 const iconButtonsClassName =
   "h-5 text-zinc-300 hover:text-zinc-100 cursor-pointer";
 
-export default function ChatInput() {
+export default function ChatInput({ onMessageSent = () => {} }) {
+  const [message, setMessage] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    onMessageSent(message);
+    setMessage("");
+  }
+
   return (
     <div className="m-4 pr-4 flex items-center bg-zinc-600 rounded-lg">
       <FontAwesomeIcon
         className="h-5 font-bold px-4 py-3 text-zinc-400 hover:text-zinc-200 cursor-pointer"
         icon={faCirclePlus}
       />
-      <input
-        className="w-4 bg-zinc-600 flex-grow focus:outline-none"
-        placeholder="Message #Channel 1"
-      />
+      <form className="flex-grow" onSubmit={onSubmit}>
+        <input
+          type="text"
+          className="w-full bg-zinc-600 flex-grow focus:outline-none"
+          placeholder="Message #Channel 1"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+      </form>
       <div className="flex gap-4 items-center">
         <FontAwesomeIcon className={iconButtonsClassName} icon={faGift} />
         <FontAwesomeIcon className={iconButtonsClassName} icon={faImage} />
