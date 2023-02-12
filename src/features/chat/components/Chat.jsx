ec7fba3f2ddbaa57e10c35bde2ad9d2f2d26a57b
Dani <24252795+SGD14@users.noreply.github.com>
2023-02-12 12:58:40 +0100

    Add chat DateLine component

--- src/features/chat/components/Chat.jsx
+++ src/features/chat/components/Chat.jsx
@@ -1,6 +1,11 @@
 import ChatInput from "./ChatInput";
+import DateLine from "./DateLine";
 import HeadMessage from "./HeadMessage";
 import NormalMessage from "./NormalMessage";
+
+const areSameDate = (date1, date2) =>
+  `${date1.getDate()}-${date1.getMonth()}-${date1.getFullYear()}` ===
+  `${date2.getDate()}-${date2.getMonth()}-${date2.getFullYear()}`;
 
 export default function Chat({
   messages = [],
@@ -11,7 +16,30 @@ export default function Chat({
     <div className="bg-zinc-700 flex-grow flex flex-col">
       <div className="flex-grow flex flex-col justify-end">
         {messages.map((message, index) => {
-          if (index === 0 || message.sender !== messages[index - 1].sender)
+          if (index === 0) {
+            return (
+              <>
+                <DateLine date={new Date(message.createDate)} />
+                <HeadMessage key={message._id} message={message} />
+              </>
+            );
+          }
+
+          if (
+            !areSameDate(
+              new Date(message.createDate),
+              new Date(messages[index - 1].createDate)
+            )
+          ) {
+            return (
+              <>
+                <DateLine date={new Date(message.createDate)} />
+                <HeadMessage key={message._id} message={message} />
+              </>
+            );
+          }
+
+          if (message.sender !== messages[index - 1].sender)
             return <HeadMessage key={message._id} message={message} />;
 
           return <NormalMessage key={message._id} message={message} />;
---
+++ src/features/chat/components/DateLine.jsx
@@ -0,0 +1,26 @@
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export default function DateLine({ date }) {
+  return (
+    <div className="flex gap-2 items-center">
+      <div className="ml-5 flex-grow h-[1px] bg-zinc-600" />
+      <div className="text-xs font-bold text-zinc-400">{`${
+        monthNames[date.getMonth()]
+      } ${date.getDate()}, ${date.getFullYear()}`}</div>
+      <div className="mr-5 flex-grow h-[1px] bg-zinc-600" />
+    </div>
+  );
+}
